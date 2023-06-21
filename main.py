c8a92524d50c196c2b17d6abe55c670a8930456b
Abdelrahman Mohamed Ali <88903965+AElomeiri@users.noreply.github.com>
2023-06-21 15:26:13 +0400

    Improve the progran by offering login and registration with two separate functions and saving all registration info within a single file.

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
+# Register fun
+def register():
+    userName = input('User Name : ')
+    password = input('Password : ')
+    with open('passInfo.txt', 'a') as f:  # Create a new file if it was not already exist and insert inside it the registration info.
+        f.write('\n' + userName + '\n')
+        f.write(password)
+        f.close()
+    print('''Done
+            ---------------------''')
+
+
+# Login fun
+def login():
+    userName = input('User Name : ')
+    password = input('Password : ')
+    with open('passInfo.txt', 'r') as f:  # Open the passInfo.txt file and search for the registration info inside it.
+        flag = True
+        while flag: # A While loop to loop over all contents of the file.
+            buffer = f.readline()
+            if buffer != '':
+                if (buffer.strip() == userName) and (f.readline().strip() == password):
+                    print('Successfully Login')
+                    break
+            else:
+                flag = False
+
+        if not flag:
+            print('Wrong login information')
+        f.close()
+
 
 # This fun display the main menu that tells the user the options.
 def mainMenu():
     print("---Welcome---")
-    print("1: Register \n2: Login")
+    print("1: Register \n2: Login \n3: Exist")
     choice = input("You choice >> ")
-    # First check if the user's choice is register (1).
-    if choice == '1':
-        userName = input('User Name : ')
-        password = input('Password : ')
-        # Create a new file and name it like the username to insert inside the register info.
-        fileName = userName + '.txt'
-        with open(fileName, 'w') as f:
-            f.write(userName + '\n')
-            f.write(password)
-            f.close()
-        print('''Done
-        ---------------------''')
-        # Rerun the fun so if the user want to register again or login.
-        mainMenu()
-    # The user's choice is login (2), then the blew code well work.
+    if choice == '1':  # First check if the user's choice is register (1).
+        register()
+        mainMenu()  # Rerun the fun so if the user want to register again or login.
+    elif choice == '2':  # The user's choice is login (2), then the blew code well work.
+        login()
     else:
-        userName = input('User Name : ')
-        password = input('Password : ')
-        # Open a file which it's name like the username and search for the register info inside it.
-        fileName = userName + '.txt'
-        with open(fileName, 'r') as f:
-            if (f.readline().strip() == userName) and (f.readline().strip() == password):
-                print('Successfully Login')
-            else:
-                print('Wrong login information')
-            f.close()
+        exit()
 
 
 if __name__ == "__main__":
